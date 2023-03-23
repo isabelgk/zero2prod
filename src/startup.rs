fbc311b77cba6df70203046107910531386862fa
Isabel Kaspriskie <mail@isabelgk.com>
2023-03-23 13:31:41 -0400

    Finish telemetry chapter

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,16 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
+use actix_web::{dev::Server, web::{Data, get, post}, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
+            .wrap(TracingLogger::default())
+            .route("/health_check", get().to(health_check))
+            .route("/subscriptions", post().to(subscribe))
             .app_data(db_pool.clone())
     })
     .listen(listener)?
